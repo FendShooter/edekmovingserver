5db233c969d65742a00989714a17fc9e176d9b05
FendShooter <virgile.dokouvi@gmail.com>
2020-10-19 20:50:00 -0400

    ready to deploy

---
+++ nodemailer.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SENDER_EMAIL, // generated ethereal user
+      pass: process.env.SENDER_PASSWORD, // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // send mail with defined transport object
+  let message = {
+    from: process.env.SENDER_EMAIL, // sender address
+    to: process.env.RECEIVER_EMAIL, // list of receivers
+    subject: 'Quote', // Subject line
+    // text: options.message, // plain text body
+    html: options.html,
+  };
+  // console.log(message);
+  // const info = await transporter.sendMail(message);
+
+  // console.log('Message sent: %s', info.messageId);
+};
+
+module.exports  = sendEmail;
