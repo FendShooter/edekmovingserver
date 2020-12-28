d00b402dcfa8f30824de045861a4f173ca0f19e4
FendShooter <virgile.dokouvi@gmail.com>
2020-12-28 14:32:17 -0500

    emails essues fixeddddd

--- nodemailer.js
+++ nodemailer.js
@@ -10,9 +10,9 @@ const sendEmail = async (options) => {
       user: process.env.SENDER_EMAIL, // generated ethereal user
       pass: process.env.SENDER_PASSWORD, // generated ethereal password
     },
-    // tls: {
-    //   rejectUnauthorized: false,
-    // },
+    tls: {
+      rejectUnauthorized: true,
+    },
   });
 
   // send mail with defined transport object
