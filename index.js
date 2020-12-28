dc885cd2b3cf11201df5d9539fc8295a5e57d08a
FendShooter <virgile.dokouvi@gmail.com>
2020-12-28 13:47:40 -0500

    email essues fixedd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(helmet());
-app.use(cors({ origin: 'https://edekmoving.com/' }));
+app.use(cors({ origin: 'https://edekmoving.com' }));
 
 //routes
 app.use('/', router);
