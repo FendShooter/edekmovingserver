70ea4765b0f5ad94178f60f765401296731de833
FendShooter <virgile.dokouvi@gmail.com>
2021-01-03 16:56:55 -0500

    fixed bugs of ema

--- controller/controller.js
+++ controller/controller.js
@@ -74,7 +74,7 @@ exports.postQuote = async (req, res, next) => {
            ${listofItems(quote.listItems)}</span> </div>
           `,
     };
-    sendEmail(options);
+    await sendEmail(options);
 
     res.status(201).send({ success: true });
   } catch (error) {
